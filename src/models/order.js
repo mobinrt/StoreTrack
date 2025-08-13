5400398278badf1eaa26c25680df9c5a7b102f1f
mobinrt <thisizzmobinrt@gmail.com>
2025-08-13 19:38:34 +0330

    fix errors in model due to adding custom id

--- src/models/order.js
+++ src/models/order.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const orderItemSchema = new mongoose.Schema({
-  item: { type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: true },
+  item: { type: String, required: true },
   quantity: { type: Number, required: true, min: 1 }
 });
 
 const orderSchema = new mongoose.Schema({
+  orderId: { type: String, required: true, unique: true },
   items: { type: [orderItemSchema], required: true },
   status: { type: String, enum: ['waiting', 'sent', 'canceled'], default: 'waiting' }
 }, { timestamps: true });
--- src/models/stockHistory.js
+++ src/models/stockHistory.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const stockHistorySchema = new mongoose.Schema({
-  item: { type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: true },
+  item: { type: String, required: true },
   changeType: { type: String, enum: ['in', 'out'], required: true },
   quantity: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now }
